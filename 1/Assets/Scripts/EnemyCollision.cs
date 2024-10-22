82b4109c2da9a24eca5f2755c0ec6b7ca7a1a660
To <132835392+CNMarkham@users.noreply.github.com>
2024-10-22 18:22:55 -0400

    72
    72

---
+++ 1/Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 1/Assets/Scripts/Throwable.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+{
+    public Vector3 offset;
+    public int throwableCounter;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ThrowingObject"))
+        {
+            Destroy(collision.gameObject);
+            throwableCounter += 1;
+        }
+    }
+
+
+
+    public GameObject ObjectThrown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (throwableCounter <= 0)
+            {
+
+
+                offset = transform.localScale.x * new Vector3(1, 0, 0);
+
+                Vector3 throwablePosition = transform.position + offset;
+
+
+                Instantiate(ObjectThrown, throwablePosition, transform.rotation);
+                throwableCounter -= 1;
+            }
+
+        }
+        }
+
+
+
+}
+
+
