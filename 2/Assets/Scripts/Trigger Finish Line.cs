be1ab9cc0400359b28758104157a6f36b64cd5d1
Madelyn <132835392+CNMarkham@users.noreply.github.com>
2024-12-10 18:21:41 -0500

    88

--- 2/Assets/Scripts/Trigger Finish Line.cs
+++ 2/Assets/Scripts/Trigger Finish Line.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerFinishLine : MonoBehaviour
+
 {
+    public CheckpointCounter checkpointTracker;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             print("You Win!");
+            if (checkpointTracker.triggeredCheckpoints == checkpointTracker.numberOfCheckpoints)
+            {
+                print("You Win");
+            }
+;
         }
     }
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
